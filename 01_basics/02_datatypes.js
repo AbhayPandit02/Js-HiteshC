52688b45138e1211cfe479cd81f3c7a8cf22745f
AbhayPandit02 <160315483+AbhayPandit02@users.noreply.github.com>
2024-08-07 04:28:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all Js code as newer version 
+
+//alert(3+3)// we are using nodejs, not browser, ideaally the output should be 6 but in node it is not possible. There does exist a different method to use alert in node 
+
+console.log("3+3") // Code readability should be high 
+console.log(3-3)
+
+/*
+  DATA TYPES
+
+  //number --> range 2 to the power 53
+  // bigint
+  //stinrg=""
+  //boolena = true/false
+  //null , this is a datatype and a stand alone value too {it is a representation of empty value}
+  // undefined, when value hasn't been defined 
+  //symbol --> uniquness 
+
+*/
+
+  //OBJECT 
+   let age=18
+
+
+  console.log(typeof "Hitesh"); //typeof gives the type of what we have written 
+  console.log(typeof age);
+  
+  
+
+
+
+
+
+
+
