92ee192cd698b5e77f0c1b5805f4cd6e8da18cc5
AbhayPandit02 <160315483+AbhayPandit02@users.noreply.github.com>
2024-08-07 04:31:41 +0000

    Js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=911911
+let accountEmail="gogo@gmail.com"
+var accountPassword="KalaJadu_12"
+ accountCity="Bhopal"
+
+// accountCity="Indore"
+// accountEmail="gogo.com"
+// accountPassword="Kalu_123"
+
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
+console.log(accountCity)
+console.log(accountCity);
+
